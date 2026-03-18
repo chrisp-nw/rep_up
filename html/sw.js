ee9adf92c13854e93fca0caa41429380d7296e9b
chrisp-nw <christian.pfaff@gmail.com>
2026-03-18 17:08:55 +0100

    Create sw.js
    added service worker script

---
+++ html/sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('rep_up').then(cache => cache.addAll(['./rep_up.html', './manifest.json']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
